a659b2cf35d8d03ebf16bc3c1c7fbbdcbfc42575
Ryan Whaley <whaleyr@pharmgkb.org>
2018-10-22 10:59:07 -0700

    Fix policies test

--- test/pharmgkb/policies_test.js
+++ test/pharmgkb/policies_test.js
@@ -1,33 +1,25 @@
 module.exports = {
-	'PharmGKB Policy Page test': function(browser) {
-    	browser.url(browser.launchUrl);
+  'PharmGKB Policy Page test': function(browser) {
+    browser.url(browser.launchUrl);
 
-    	browser.waitForElementPresent('.home-page');
-    	browser.assert.title('PharmGKB');
+    browser.waitForElementPresent('.home-page');
+    browser.assert.title('PharmGKB');
 
-        browser.url(function (result) {
-            browser.resizeWindow(1280, 800);
-            browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
-        });
+    browser.click('.page-footer .row > :nth-child(2) > :nth-child(3)');
+    browser.waitForElementPresent('.html-container p:nth-of-type(2)');
+    browser.assert.urlContains('/policies');
+    browser.assert.containsText('.html-container p:nth-of-type(1)', 'read the following policies');
 
-        browser.click('.page-footer .row > :nth-child(2) > :nth-child(3)');
-        browser.waitForElementPresent('.html-container p:nth-of-type(2)');
-        browser.assert.urlContains('/policies');
-        browser.assert.containsText('.html-container p:nth-of-type(1)', 'read the following policies');
-        browser.url(function (result) {
-            browser.resizeWindow(1280, 800);
-            browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
-        });
+    browser.click('.html-container > :nth-child(2) a');
+    browser.assert.urlContains('/dataUsagePolicy');
+    browser.waitForElementPresent('#terms-and-conditions-of-use');
+    browser.assert.containsText('div.container h1', 'Data Usage Policy');
 
-        browser.click('.html-container > :nth-child(2) a');
-        browser.assert.urlContains('/dataUsagePolicy');
-        browser.waitForElementPresent('#terms-and-conditions-of-use');
-      	browser.assert.containsText('.static h1', 'Data Usage Policy');
-        browser.url(function (result) {
-            browser.resizeWindow(1280, 800);
-            browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
-        });
+    browser.url(function (result) {
+      browser.resizeWindow(1280, 800);
+      browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
+    });
 
-      	browser.end();
+    browser.end();
   }
 };
