b706ad68333c2730d68bd77e268add31e7ee98af
Ryan Whaley <whaleyr@pharmgkb.org>
2018-10-22 11:03:20 -0700

    Fix search test

--- test/pharmgkb/search_test.js
+++ test/pharmgkb/search_test.js
@@ -1,42 +1,38 @@
 module.exports = {
-	'PharmGKB Search Page test': function(browser) {
-		browser.url(browser.launchUrl);
-		browser.waitForElementPresent('.home-page');
-		browser.assert.title('PharmGKB');
-		browser.waitForElementPresent('.animated-line');
-        browser.url(function (result) {
-            browser.resizeWindow(1280, 800);
-            browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
-        });
+  'PharmGKB Search Page test': function(browser) {
+    browser.url(browser.launchUrl);
+    browser.waitForElementPresent('.home-page');
+    browser.assert.title('PharmGKB');
+    browser.waitForElementPresent('.animated-line');
 
-		browser.setValue('.search-input', [' ', browser.Keys.ENTER]);
-    	browser.waitForElementPresent('.results-list');
-    	browser.assert.urlContains('query=%20');
-    	browser.assert.containsText('.results-list', 'No results matched your query.');
-        browser.url(function (result) {
-            browser.resizeWindow(1280, 800);
-            browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
-        });
+    browser.setValue('.search-input', [' ', browser.Keys.ENTER]);
+    browser.waitForElementPresent('.results-list');
+    browser.assert.urlContains('query=%20');
+    browser.assert.containsText('.results-list', 'No results matched your query.');
+    browser.url(function (result) {
+      browser.resizeWindow(1280, 800);
+      browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
+    });
 
-        browser.setValue('.search-input', ['warf', browser.Keys.ENTER]);
-    	browser.waitForElementPresent('.results-list');
-    	browser.assert.urlContains('query=warf');
-    	browser.assert.containsText('.results-list', 'results that have "warf" in the title');
-    	browser.waitForElementPresent('.chemical-list-item:nth-of-type(1)');
-        browser.url(function (result) {
-            browser.resizeWindow(1280, 800);
-            browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
-        });
+    browser.setValue('.search-input', ['warf', browser.Keys.ENTER]);
+    browser.waitForElementPresent('.results-list');
+    browser.assert.urlContains('query=warf');
+    browser.assert.containsText('.results-list', 'results that have "warf" in the title');
+    browser.waitForElementPresent('.chemical-list-item');
+    browser.url(function (result) {
+      browser.resizeWindow(1280, 800);
+      browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
+    });
 
-        browser.click('.chemical-list-item:nth-of-type(1) a');
-        browser.waitForElementPresent('.chemicalStructure div img');
-        browser.assert.urlContains('chemical');
-        browser.assert.title('warfarin - Overview | PharmGKB');
-        browser.url(function (result) {
-            browser.resizeWindow(1280, 800);
-            browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
-        });
+    browser.click('.chemical-list-item a');
+    browser.waitForElementPresent('.chemicalStructure div img');
+    browser.assert.urlContains('chemical');
+    browser.assert.title('warfarin - Overview | PharmGKB');
+    browser.url(function (result) {
+      browser.resizeWindow(1280, 800);
+      browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
+    });
 
-        browser.end();
-	}
+    browser.end();
+  }
 };
