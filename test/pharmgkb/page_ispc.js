14ba30eabd0847a66306b22916d59694dd0a768e
Ryan Whaley <whaleyr@pharmgkb.org>
2018-10-22 11:24:47 -0700

    Fix ISPC page test

--- test/pharmgkb/page_ispc.js
+++ test/pharmgkb/page_ispc.js
@@ -1,14 +1,15 @@
 module.exports = {
-    'PharmGKB ISPC Page test': function (browser) {
-        browser.url(browser.launchUrl + '/page/ispc');
+  'PharmGKB ISPC Page test': function (browser) {
+    browser.url(browser.launchUrl + '/page/ispc');
 
-        browser.waitForElementPresent('.static');
-        browser.assert.containsText('section.static h1', 'ISPC - International SSRI Pharmacogenomics Consortium');
-        browser.url(function (result) {
-            browser.resizeWindow(1280, 800);
-            browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
-        });
+    browser.waitForElementPresent('.static');
+    browser.assert.containsText('div.container h1', 'ISPC - International SSRI Pharmacogenomics Consortium');
+    browser.expect.element('#ispc-team').to.be.visible;
+    browser.url(function (result) {
+      browser.resizeWindow(1280, 800);
+      browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
+    });
 
-        browser.end();
-    }
+    browser.end();
+  }
 };
