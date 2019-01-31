9d062ee0e7481737ecef0a5a9548c9af6323d654
Ryan Whaley <whaleyr@pharmgkb.org>
2019-01-31 09:38:44 -0800

    Fix drug label test
    - no screen shot of external websites, not useful
    - fix URL path for labels

--- test/pharmgkb/drug_label_test.js
+++ test/pharmgkb/drug_label_test.js
@@ -1,30 +1,26 @@
 module.exports = {
-	'PharmGKB Drug Label Page test': function (browser) {
-		browser.url(browser.launchUrl + '/label/PA166104782');
+  'PharmGKB Drug Label Page test': function (browser) {
+    browser.url(browser.launchUrl + '/labelAnnotation/PA166104782');
 
-		browser.waitForElementVisible('.label-detail');
-        browser.assert.title('Annotation of FDA Label for azathioprine and TPMT | PharmGKB');
-        browser.assert.urlContains('/label/');
-        browser.url(function (result) {
-            browser.resizeWindow(1280, 800);
-            browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
-        });
+    browser.waitForElementVisible('.label-detail');
+    browser.assert.title('Annotation of FDA Label for azathioprine and TPMT | PharmGKB');
+    browser.assert.urlContains('/labelAnnotation/');
+    browser.url(function (result) {
+      browser.resizeWindow(1280, 800);
+      browser.saveScreenshot(process.env.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
+    });
 
-        browser.click('.more-info-links div:nth-of-type(1) a');
+    browser.click('.more-info-links div:nth-of-type(1) a');
 
-        browser.windowHandles(function(result) {
-        	var newWindow;
-        	this.verify.equal(result.value.length, 2, 'There should be 2 windows open');
-        	newWindow = result.value[1];
-        	this.switchWindow(newWindow);
-        });
-        browser.waitForElementVisible('.drug-information:nth-of-type(1)');
-        browser.assert.urlContains('/dailymed/');
-        browser.url(function (result) {
-            browser.resizeWindow(1280, 800);
-            browser.saveScreenshot(this.SCREENSHOT_PATH + '/' + result.value.substring(24) + '.png');
-        });
+    browser.windowHandles(function(result) {
+      var newWindow;
+      this.verify.equal(result.value.length, 2, 'There should be 2 windows open');
+      newWindow = result.value[1];
+      this.switchWindow(newWindow);
+    });
+    browser.waitForElementVisible('div.panel-info');
+    browser.assert.urlContains('fda.gov');
 
-        browser.end();
-	}
+    browser.end();
+  }
 };
