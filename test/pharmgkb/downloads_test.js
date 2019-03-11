2294e5a09e2c54a72c1463472ed3383cad021fa2
Ryan Whaley <whaleyr@pharmgkb.org>
2019-03-11 11:11:54 -0700

    Fix genes label selector

--- test/pharmgkb/downloads_test.js
+++ test/pharmgkb/downloads_test.js
@@ -11,7 +11,7 @@ module.exports = {
       .assert.containsText('.downloads__card:nth-of-type(1) .downloads__card_text ul li', 'annotations.zip')
       .assert.containsText('.downloads__card:nth-of-type(2) .downloads__card_text ul li', 'relationships.zip')
       .assert.containsText('div.row:nth-of-type(3) .downloads__card:nth-of-type(2) .downloads__card_text ul li', 'drugLabels.zip')
-      .assert.containsText('section:nth-of-type(2) div.row:nth-of-type(2) .downloads__card:nth-of-type(1) .downloads__card_text ul li', 'genes.zip');
+      .assert.containsText('div.downloads div section:nth-of-type(3) div.row .downloads__card_text ul li', 'genes.zip');
     helpers.screenshot(browser, path);
 
     browser.end();
