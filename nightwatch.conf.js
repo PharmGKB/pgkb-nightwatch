9b9ff5a154c5dda65885acae521030d76c72ac4d
Ryan Whaley <whaleyr@pharmgkb.org>
2019-03-11 12:35:05 -0700

    Slim down nightwatch config
    - don't need selenium as of nightwatch 1.x
    - clean up screenshot config settings (and default to .env path)
    - don't use a mobile user-agent string

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,22 +1,16 @@
 require('env2')('.env'); // optionally store your environment variables in .env
-const seleniumServer = require("selenium-server");
 const chromedriver = require("chromedriver");
-const SCREENSHOT_PATH = 'screenshots';
+const SCREENSHOT_PATH = process.env.SCREENSHOT_PATH || 'screenshots';
 
-const config = {
+module.exports = {
   "src_folders": [
     "test/pharmgkb"
   ],
   "output_folder": "./node_modules/nightwatch/reports", // reports (test outcome) output by Nightwatch
-  "selenium": {
+  "webdriver": {
     "start_process": true,
-    "server_path": seleniumServer.path,
-    "log_path": "",
-    "host": "127.0.0.1",
-    "port": 4444,
-    "cli_args": {
-      "webdriver.chrome.driver" : chromedriver.path
-    }
+    "server_path": chromedriver.path,
+    "port": 9515,
   },
   "test_workers" : {"enabled" : true, "workers" : 3},
   "test_settings": {
@@ -35,8 +29,7 @@ const config = {
         "browserName": "chrome",
         "chromeOptions": {
           "args": [
-            "headless",
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3"
+            "headless"
           ]
         },
         "javascriptEnabled": true,
@@ -49,32 +42,6 @@ const config = {
     "fe-beta": {
       "launch_url": "https://next-beta.pharmgkb.org",
     }
-  }
+  },
+  SCREENSHOT_PATH,
 };
-
-module.exports = config;
-
-function padLeft (count) { // theregister.co.uk/2016/03/23/npm_left_pad_chaos/
-  return count < 10 ? '0' + count : count.toString();
-}
-
-var FILECOUNT = 0; // "global" screenshot file count
-/**
- * The default is to save screenshots to the root of your project even though
- * there is a screenshots path in the config object above! ... so we need a
- * function that returns the correct path for storing our screenshots.
- * While we're at it, we are adding some meta-data to the filename, specifically
- * the Platform/Browser where the test was run and the test (file) name.
- */
-function imgpath (browser) {
-  var a = browser.options.desiredCapabilities;
-  var meta = [a.platform];
-  meta.push(a.browserName ? a.browserName : 'any');
-  meta.push(a.version ? a.version : 'any');
-  meta.push(a.name); // this is the test filename so always exists.
-  var metadata = meta.join('~').toLowerCase().replace(/ /g, '');
-  return SCREENSHOT_PATH + metadata + '_' + padLeft(FILECOUNT++) + '_';
-}
-
-module.exports.imgpath = imgpath;
-module.exports.SCREENSHOT_PATH = SCREENSHOT_PATH;
