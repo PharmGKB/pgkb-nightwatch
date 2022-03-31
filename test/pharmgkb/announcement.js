a9003c7d13078724b91d337d4a2d01d3f2335e2c
Ryan Whaley <whaleyr@pharmgkb.org>
2022-03-31 13:00:32 -0700

    test: fix tour dismissal test

--- test/pharmgkb/announcement.js
+++ test/pharmgkb/announcement.js
@@ -10,11 +10,16 @@ module.exports = {
 
         browser
             .url(browser.baseUrl)
-            .click('.banner__close') // dismiss the banner
-            .click('.stat.pathways') // go to the pathways list page
-            .click('.listItem__name__title a'); // go to the first pathway page
-        browser
-            .expect.element('.banner__close').to.not.be.present; // the banner should not be there
+            .click('.banner__close')                        // dismiss the banner
+            .waitForElementNotPresent('.banner')      // wait for the banner to go away
+            .click('.stat.pathways')                        // go to the pathways list page
+            .click('a.brand')                               // navigate back to homepage
+            .assert.not.elementPresent('.banner')  // banner should not show again
+            .click('.stat.pathways')                        // go to pathway list
+            .click('.listItem__name__title a')              // go to the first pathway page
+            .assert.elementPresent('.banner');     // this banner will show because there are multiple tour
+                                                            // banners and this is a different one than the one that
+                                                            // was dismissed
         browser.end();
     }
 };
