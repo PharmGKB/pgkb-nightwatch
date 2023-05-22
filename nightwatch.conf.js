4efc1c197846c18b435d259cf24a583d5cdc006c
Ryan Whaley <whaleyr@pharmgkb.org>
2023-05-22 11:23:55 -0700

    chore: update dependencies

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -76,11 +76,7 @@ module.exports = {
       },
       webdriver: {
         start_process: true,
-        server_path: '',
-        cli_args: [
-          // very verbose geckodriver logs
-          // '-vv'
-        ]
+        server_path: 'node_modules/geckodriver/.bin/geckodriver'
       }
     },
 
